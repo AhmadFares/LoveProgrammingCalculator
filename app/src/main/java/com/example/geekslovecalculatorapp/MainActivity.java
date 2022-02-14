ac69fa7a2758df242cf63a87f5d50bebf6adb1ca
Ahmad Fares <ahmaddfaress@Ahmads-MacBook-Air.local>
2022-02-14 17:00:06 +0200

    Fixed the calculator, added an error when no name has been entered, added 6 icons for languages, but still require fixing

--- app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
@@ -6,15 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    EditText Name;
+    TextView P;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        P=(TextView) findViewById(R.id.textView);
+        Name=(EditText) findViewById(R.id.Username);
 
         Spinner spinner= findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.Lang, android.R.layout.browser_link_context_header);
@@ -23,14 +32,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setOnItemSelectedListener(this);
     }
 
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
-String text= parent.getItemAtPosition(pos).toString();
+    String text= parent.getItemAtPosition(pos).toString();
 
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+    public void Calculate(View V){
+        Random rand = new Random();
+        int i= rand.nextInt(101);
+        String s= Name.getText().toString();
+        if(s.matches("")) {
+            P.setText("Error");
+        }
+else {
+            P.setText(i + "%");
+        }
     }
 }
